a1a41aed461f2eb1e5648a76fd715faaabc4b424
Leon Mergen <leon@solatis.com>
2017-09-23 16:54:25 +0200

    Implements backoff/retry mechanism

--- src/main/scala/com/quasardb/spark/rdd/Util.scala
+++ src/main/scala/com/quasardb/spark/rdd/Util.scala
@@ -1,14 +1,47 @@
 package com.quasardb.spark.rdd
 
+import java.sql.Timestamp
+
+import scala.collection.JavaConversions._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import scala.concurrent.duration._
-import com.quasardb.spark.Configuration
+import scala.concurrent.{Await, Future}
+import odelay.Timer
+
+import net.quasardb.qdb._
+
+import com.quasardb.spark.rdd.ts.DoubleRDD
+
+import retry._
+import retry.Success
 
 object Util {
 
-  def withRetries[T](f: () => T) {
-    println("retrying...")
-  }
+  def insertDoubles(
+    uri: String,
+    table: String,
+    column: String,
+    values: Iterator[(Timestamp, Double)]): Unit = {
 
+    var collection = new QdbDoubleColumnCollection(column)
+    collection.addAll(values.map(DoubleRDD.toJava).toList)
+
+    implicit val success = Success[Boolean](_ == true)
+    implicit val timer = odelay.Timer.default
+
+    val future = retry.Backoff(8, 50.millis)(timer) { () =>
+
+      try {
+        new QdbCluster(uri).timeSeries(table).insertDoubles(collection)
+        Future.successful(true)
+      } catch {
+
+        // Thrown in case of race condition
+        case e: QdbOperationException =>
+          Future.failed(e)
+      }
+    }
+
+    Await.result(future, 30.second)
+  }
 }
--- src/main/scala/com/quasardb/spark/rdd/ts/DoubleRDDFunctions.scala
+++ src/main/scala/com/quasardb/spark/rdd/ts/DoubleRDDFunctions.scala
@@ -1,27 +1,16 @@
 package com.quasardb.spark.rdd.ts
 
 import java.sql.Timestamp
-import scala.collection.JavaConversions._
 import org.apache.spark.rdd.RDD
-
-import com.quasardb.spark.Configuration
 import com.quasardb.spark.rdd.Util
-import com.quasardb.spark.rdd.ts.DoubleRDD
-import net.quasardb.qdb._
 
 class DoubleRDDFunctions[A <: (Timestamp, Double)](data: RDD[A]) extends Serializable {
 
   def toQdbDoubleColumn(
     uri: String,
     table: String,
-    column: String,
-    configuration: Configuration = new Configuration) : Unit = {
+    column: String) : Unit = {
 
-    data.foreachPartition { partition =>
-      var collection = new QdbDoubleColumnCollection(column)
-      collection.addAll(partition.map(DoubleRDD.toJava).toList)
-
-      new QdbCluster(uri).timeSeries(table).insertDoubles(collection)
-    }
+    data.foreachPartition { partition => Util.insertDoubles(uri, table, column, partition) }
   }
 }
